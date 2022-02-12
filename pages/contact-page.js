04e9dc3c92caff7b7d816a1a9a2b4106444af1eb
aozou99 <hellostacysmom@gmail.com>
2022-02-12 12:27:50 +0900

    initial commit

---
+++ pages/contact-page.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Layout from "../components/Layout";
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold"> Contact info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src={"/avator.png"}
+            width={60}
+            height={60}
+            alt="avator"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="font-bold">Address</p>
+          <p className="text-xs mt-2 text-gray-600">city A</p>
+          <p className="font-bold mt-3">E-mail</p>
+          <p className="text-xs mt-2 text-gray-600">abc@gmail.com</p>
+          <p className="font-bold mt-3">Phone</p>
+          <p className="text-xs mt-2 text-gray-600">000-123-456</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
---
+++ pages/posts/[id].js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+import React from "react";
+
+const Post = ({ post }) => {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID : "}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.title}</p>
+      <p className="px-10">{post.body}</p>
+      <Link href={"/blog-page"}>
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-3"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+};
+export default Post;
+
+export const getStaticPaths = async () => {
+  const paths = await getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { post: post } = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+};
